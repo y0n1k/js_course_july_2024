7fe9d9a552eba84adc105125350428868eb9f121
y0n1k <yan.pekarski@gmail.com>
2024-08-27 13:22:45 +0300

    dz 7 done

--- lesson7/main.js
+++ lesson7/main.js
@@ -108,17 +108,134 @@ console.log(clientArraySortedDes)
 // -- increaseMaxSpeed (newSpeed) - яка підвищує значення максимальної швидкості на значення newSpeed
 // -- changeYear (newValue) - змінює рік випуску на значення newValue
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
+console.log('')
+console.log('ЗАВДАННЯ №6')
+console.log('')
 
-function Car(model, producer, release, max_speed, engine_volume){
+function Car(model, producer, release, max_speed, engine_volume) {
     this.model = model;
-    this.producer=producer;
-    this.release=release;
-    this.max_speed=max_speed;
-    this.engine_volume=engine_volume;
-    this.drive = function drive (){
-        console.log(`їдемо зі швидкістю ${max_speed} на годину`)
+    this.producer = producer;
+    this.release = release;
+    this.max_speed = max_speed;
+    this.engine_volume = engine_volume;
+    this.drive = function () {
+        console.log(`їдемо зі швидкістю ${this.max_speed} на годину`);
     }
-    this.info = function info ()
+    this.info = function () {
+        for (const element in this) {
+            console.log(element, this[element]);
+        }
+    }
+    this.increaseMaxSpeed = function (newSpeed) {
+        this.max_speed += newSpeed;
+    }
+    this.changeYear = function (newValue) {
+        this.release = newValue;
+    }
+    this.addDriver = function (driver) {
+        this.driver = driver
+    }
 }
 
+let car1 = new Car('Octavia', 'Skoda', 1996, 230, 2);
+console.log(car1);
 
+car1.increaseMaxSpeed(15)
+car1.changeYear(2006)
+car1.addDriver({name: 'David', age: 31})
+car1.info()
+car1.drive()
+
+//створити клас Car
+console.log('')
+console.log('ЗАВДАННЯ №7')
+console.log('')
+
+class CarClass {
+    constructor(model, producer, release, max_speed, engine_volume) {
+        this.model = model;
+        this.producer = producer;
+        this.release = release;
+        this.max_speed = max_speed;
+        this.engine_volume = engine_volume;
+    }
+
+    drive() {
+        console.log(`їдемо зі швидкістю ${this.max_speed} на годину`);
+    }
+
+    increaseMaxSpeed(newSpeed) {
+        this.max_speed += newSpeed;
+    }
+
+    changeYear(newValue) {
+        this.release = newValue;
+    }
+
+    info() {
+        for (const element in this) {
+            console.log(element, this[element]);
+        }
+    }
+
+    addDriver(driver) {
+        this.driver = driver;
+    }
+}
+
+let car2 = new CarClass('Lanos', 'Daewoo', 1997, 170, 1.5)
+console.log(car2)
+
+car2.addDriver({name: 'Oleg', age: '54'})
+car2.drive()
+car2.changeYear(2000)
+car2.increaseMaxSpeed(10)
+car2.info()
+
+// -створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги. Створити масив з 10 попелюшок.
+// Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
+//     За допомоги циклу знайти яка попелюшка повинна бути з принцом.
+//     Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку
+console.log('')
+console.log('ЗАВДАННЯ №8')
+console.log('')
+
+class Cinderella {
+    constructor(name, age, footSize) {
+        this.name = name;
+        this.age = age;
+        this.footSize = footSize;
+    }
+}
+class Prince {
+    constructor(name,age,shoeSize) {
+        this.name = name;
+        this.age = age;
+        this.shoeSize = shoeSize
+    }
+}
+
+let cinderellaArray = [
+    new Cinderella('Margo',22,38),
+    new Cinderella('Julie',20,36),
+    new Cinderella('Alice',25,38),
+    new Cinderella('Victoria',21,38),
+    new Cinderella('Ann',22,35),
+    new Cinderella('Liz',24,37),
+    new Cinderella('Sophia',26,39),
+    new Cinderella('Katherine',21,36),
+    new Cinderella('Lilith',19,35),
+    new Cinderella('Suzie',23,36),
+]
+
+console.log(cinderellaArray)
+
+let prince = new Prince('John', 24, 37)
+
+console.log(prince)
+
+for (const cinderella of cinderellaArray) {
+    if (cinderella.footSize===prince.shoeSize){
+        console.log('The Cinderella is', cinderella.name, cinderella)
+    }
+}
