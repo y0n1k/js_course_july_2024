cab31b04eb60cc8dd40b80a086cf8d991901ec66
y0n1k <yan.pekarski@gmail.com>
2024-09-03 22:34:07 +0300

    dz 11 DONE

--- lesson11/main.js
+++ lesson11/main.js
@@ -34,3 +34,44 @@ fetch ('https://dummyjson.com/carts')
             cartsDiv.appendChild(div)
         }
     })
+
+// - взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути список під час відображення.
+let olFunc = function(){
+    return document.createElement('ol');
+}
+
+const recipesDiv = document.getElementById('recipes')
+fetch('https://dummyjson.com/recipes')
+    .then(value => value.json())
+    .then(recipesObject => {
+        const {recipes} = recipesObject;
+        console.log(recipes);
+        for (const recipe of recipes) {
+            const div = document.createElement('div')
+            for (const recipeKey in recipe){
+                if (Array.isArray(recipe[recipeKey])){
+                    let p = document.createElement('p')
+                    p.innerText = recipeKey;
+                    recipesDiv.appendChild(p)
+                    let ol = document.createElement('ol');
+                    let array = recipe[recipeKey];
+                    for (const item of array) {
+                        const li = document.createElement('li')
+                        li.innerText = item;
+                        ol.appendChild(li);
+                    }
+                    recipesDiv.appendChild(ol)
+                } else {
+                    const keyDiv = document.createElement('div');
+                    keyDiv.innerText = `${recipeKey}: ${recipe[recipeKey]}`;
+                    recipesDiv.appendChild(keyDiv);
+                }
+            }
+            const img = document.createElement('img')
+            img.style.width = '400px'
+            img.style.objectFit = 'cover'
+            img.src = recipe.image;
+            recipesDiv.appendChild(img)
+            recipesDiv.appendChild(div)
+        }
+    });
