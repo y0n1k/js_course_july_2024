0521d34be3925cdb1f290dbf3e0bff2f1f7434cc
y0n1k <yan.pekarski@gmail.com>
2024-09-05 15:54:42 +0300

    project started
    js files added

---
+++ project/index-js.js
@@ -0,0 +1,7 @@
+// В index.html
+// 1 отримати масив об'єктів з endpoint`а https://jsonplaceholder.typicode.com/users
+// 2 Вивести id,name всіх user в index.html. Окремий блок для кожного user.
+// 3 Додати кожному блоку кнопку/посилання , при кліку на яку відбувається перехід  на сторінку user-details.html, котра має детальну інфорацію про об'єкт на який клікнули
+
+fetch('https://jsonplaceholder.typicode.com/users')
+then()
